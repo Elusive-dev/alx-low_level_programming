256dc2997cb3219a38c8ef3d6721b3f4acc5c530
Godwin <godwineffiong16@gmail.com>
2022-03-29 16:30:56 -0700

    Fifth task

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,35 +1,31 @@
-#include <stddef.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
- * _strstr -  finds the first occurence of the substring needle
- * in the string haystack
- * @haystack: base string
- * @needle: substring
- *
- * Return: pointer to the beginning of the located substring, or
+ * _strstr - finds the first occurrence of the substring.
+ * needle in the string haystack.
+ * @haystack: entire string.
+ * @needle: substring.
+ * Return: pointer to the beginning of located substring or
  * NULL if the substring is not found.
- */
+*/
 char *_strstr(char *haystack, char *needle)
 {
-	char *n;
-	char *h;
+char *bhaystack;
+char *pneedle;
 
-	for (; *haystack != '\0'; haystack++)
-	{
-		if (*needle == *haystack)
-		{
-			n = needle;
-			h = haystack;
-			for (; *n != '\0'; n++)
-			{
-				if (*n != *h)
-					break;
-				h++;
-			}
-			if (*n == '\0')
-				return (haystack);
-		}
-	}
-	return (NULL);
+while (*haystack != '\0')
+{
+bhaystack = haystack;
+pneedle = needle;
+
+while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
+{
+haystack++;
+pneedle++;
+}
+if (!*pneedle)
+return (bhaystack);
+haystack = bhaystack + 1;
+}
+return (0);
 }
